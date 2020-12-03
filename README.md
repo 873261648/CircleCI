b7ce73f0e883a9ae8fe5a7a37af74fda38896164
guoyuchen <guoyuchen@imooc.com>
2020-12-03 19:01:03 +0800

    修改publishPath

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CircleCI
 
 Circle自动化部署
+
+[](https://873261648.github.io/CircleCI/)
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,58 @@
 <template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
     <ul>
-        <li v-for="item of list" :key="item" v-text="item"></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
+    <h3>Essential Links</h3>
     <ul>
-        <li v-for="item of evenList" :key="item" v-text="item"></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
-    <button @click="add">add</button>
-    <div id="box"></div>
-    <teleport to="#box">
-        <button>teleport</button>
-    </teleport>
-    <event @focusss="fucusss"/>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import {ref, onMounted, watch, computed} from 'vue';
-import Event from "./event.vue";
-
 export default {
-    components: {Event},
-    props: {
-        msg: {
-            type: String,
-            default: ""
-        }
-    },
-    setup(props) {
-        let list = ref([]);
-
-        const getList = () => {
-            list.value = [1, 2, 3, 4, 5, 6];
-        }
-        const add = () => {
-            list.push('4')
-        }
-
-        watch(list, (newVal, oldVal) => {
-            console.log(`list发生变化，新数据为${newVal},旧数据为${oldVal}`)
-        })
-        var evenList = computed(() => {
-            return list.value.filter((item, index) => {
-                if (index % 2 === 0) {
-                    return item
-                }
-            })
-        })
-
-        onMounted(getList)
-        return {list, add,evenList}
-    },
-    methods:{
-        fucusss(...data){
-            console.log(data)
-        }
-    }
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
