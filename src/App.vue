f1be183b417234806472bf786889569e935f40b9
guoyuchen <guoyuchen@imooc.com>
2021-07-22 16:01:10 +0800

    refs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <p v-for="item of list" :key="item">第{{item}}条</p>
+        <p v-for="item of list" :key="item" :ref="handleItem">第{{ item }}条</p>
     </div>
 </template>
 <script>
@@ -8,11 +8,20 @@ export default {
     name: 'App',
     data() {
         return {
-            list: [1, 2, 3, 4, 5]
+            list: [1, 2, 3, 4, 5],
+            itemRefs: []
         }
     },
     mounted() {
-
+        console.log(this.itemRefs)
+    },
+    methods: {
+        handleItem(el) {
+            this.itemRefs.push(el);
+            return el
+        }
+    },
+    updated() {
     }
 }
 </script>
